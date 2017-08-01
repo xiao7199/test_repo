f3b7827a413e215708525750e56925f8f9c71549
Xiao Zhang <zhang7@seas.upenn.edu>
2017-08-01 00:48:15 -0400

    key_point

---
+++ key_point_detector/model.py
@@ -0,0 +1,121 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+# https://github.com/shelhamer/fcn.berkeleyvision.org/blob/master/surgery.py
+def get_upsampling_weight(in_channels, out_channels, kernel_size):
+	"""Make a 2D bilinear kernel suitable for upsampling"""
+	factor = (kernel_size + 1) // 2
+	if kernel_size % 2 == 1:
+		center = factor - 1
+	else:
+		center = factor - 0.5
+	og = np.ogrid[:kernel_size, :kernel_size]
+	filt = (1 - abs(og[0] - center) / factor) * \
+		   (1 - abs(og[1] - center) / factor)
+	weight = np.zeros((in_channels, out_channels, kernel_size, kernel_size),
+					  dtype=np.float64)
+	weight[range(in_channels), range(out_channels), :, :] = filt
+	return torch.from_numpy(weight).float()
+
+
+class FCN32s(nn.Module):
+
+	def __init__(self, n_class=21):
+		super(FCN32s, self).__init__()
+		self.features = nn.Sequential(
+			# conv1
+			nn.Conv2d(3, 64, 3, padding=100),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(64, 64, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/2
+
+			# conv2
+			nn.Conv2d(64, 128, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(128, 128, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/4
+
+			# conv3
+			nn.Conv2d(128, 256, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(256, 256, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(256, 256, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/8
+
+			# conv4
+			nn.Conv2d(256, 512, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(512, 512, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(512, 512, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/16
+
+			# conv5
+			nn.Conv2d(512, 512, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(512, 512, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.Conv2d(512, 512, 3, padding=1),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(2, stride=2, ceil_mode=True),  # 1/32
+		)
+		self.classifier = nn.Sequential(
+			# fc6
+			nn.Conv2d(512, 4096, 7),
+			nn.ReLU(inplace=True),
+			nn.Dropout2d(),
+
+			# fc7
+			nn.Conv2d(4096, 4096, 1),
+			nn.ReLU(inplace=True),
+			nn.Dropout2d(),
+
+			# score_fr
+			nn.Conv2d(4096, n_class, 1),
+		)
+		self.upscore = nn.ConvTranspose2d(n_class, n_class, 64, stride=32,
+										  bias=False)
+		self._initialize_weights()
+
+	def _initialize_weights(self):
+		for m in self.modules():
+			if isinstance(m, nn.Conv2d):
+				m.weight.data.zero_()
+				if m.bias is not None:
+					m.bias.data.zero_()
+			if isinstance(m, nn.ConvTranspose2d):
+				assert m.kernel_size[0] == m.kernel_size[1]
+				initial_weight = get_upsampling_weight(
+					m.in_channels, m.out_channels, m.kernel_size[0])
+				m.weight.data.copy_(initial_weight)
+
+	def forward(self, x):
+		h = self.features(x)
+
+		h = self.classifier(h)
+
+		h = self.upscore(h)
+		h = h[:, :, 19:19+x.size()[2], 19:19+x.size()[3]].contiguous()
+
+		return h
+
+	def copy_params_from_vgg16(self, vgg16):
+		for l1, l2 in zip(vgg16.features, self.features):
+			if (isinstance(l1, nn.Conv2d) and
+					isinstance(l2, nn.Conv2d)):
+				assert l1.weight.size() == l2.weight.size()
+				assert l1.bias.size() == l2.bias.size()
+				l2.weight.data = l1.weight.data
+				l2.bias.data = l1.bias.data
+		for i in [0, 3]:
+			l1 = vgg16.classifier[i]
+			l2 = self.classifier[i]
+			l2.weight.data = l1.weight.data.view(l2.weight.size())
+			l2.bias.data = l1.bias.data.view(l2.bias.size())
---
+++ key_point_detector/utils.py
@@ -0,0 +1,89 @@
+import numpy as np
+import torch.utils.data as data
+import glob,os,pdb
+import cv2
+import skimage.misc
+import torch
+import torch.nn as nn
+from scipy.stats import multivariate_normal
+
+def get_gaussian_gt_label(label, sigma, img_h, img_w):
+	label_num = label.shape[0]
+	x_space = np.linspace(0, 1, img_w, endpoint=False)
+	y_space = np.linspace(0, 1, img_h, endpoint=False)
+	pos = np.zeros((img_h,img_w,2))
+	pos[:,:,0] = x_space
+	pos[:,:,1] = y_space
+	gt_label = np.zeros((label_num,img_h,img_w))
+	for i in range(label_num):
+		rv = multivariate_normal(gt_label[i,:], [[sigma, 0], [0, sigma]])
+		gt_label[i,:,:] = rv.pdf(pos)
+	return gt_label
+
+def read_gt_txt(filename):
+	f = open(filename)
+	key_point = []
+	for (iii, line) in enumerate(f):
+		if iii == 0:
+			bbox_cnt = int(line.strip())
+			continue
+		tmp = [int(t.strip()) for t in line.split()]
+		key_point.append(tmp)
+	if len(key_point) != 5:
+		return -1
+	gt_label = np.zeros((5,2))
+	for i in range(5):
+		gt_label[i,:] = key_point[i]
+	return gt_label
+
+class Dataloader(data.Dataset):
+	
+	def __init__(self, param, mode = 'train'):
+
+
+		self.data_folder = param['data_folder_name']
+		if mode == 'test':
+			self.folder_num = param['test_data_folder_name']
+		self.folder_num = len(self.data_folder)
+		self.label_data_folder = param['label_data_folder']
+		self.data_index_binbook = np.zeros((self.folder_num+1,1))
+		self.rb_vector = []
+		self.epoch = param['epoch']
+		self.img_data_path = param['img_data_path']
+		self.gt_txt_path = param['gt_txt_path']
+		self.img_h = param['img_h']
+		self.img_w = param['img_w']
+		self.mean_val = param['mean_val']
+		self.sigma = param['sigma']
+		self.img_name_list = []
+		temp_index = 0
+		for index, data_folder_nsame in enumerate(data_folder):
+			self.img_name_list[index] = glob.glob(os.path.join\
+				(self.img_data_path,data_folder_name,'*.jpg'))
+			curr_visual_size =  len(self.img_name_list[index])
+			temp_index += curr_visual_size
+			self.data_index_binbook[index+1] += temp_index
+		self.total_load_num = temp_index
+		print('finish initializing data loader')
+	
+	def __getitem__(self, index):
+
+		data_index_in_epoch = index % self.total_load_num
+		data_folder_bin_num = np.digitize(data_index_in_epoch, self.data_index_binbook)
+		data_folder_name = self.data_folder[data_folder_bin_num - 1]
+		data_start_index_in_folder = data_index_in_epoch - self.data_index_binbook[data_folder_bin_num-1]
+		img_name = self.img_name_list[index][data_start_index_in_folder]
+		img_txt_label_name = img_name[:img_name.index('.')] + '.txt'
+
+		gt_label = read_gt_txt(os.path.join(self.gt_txt_path, data_folder,img_txt_label_name))
+
+		if gt_label == -1:
+			return None,None
+		
+		img = cv2.imread(os.path.join\
+			(self.img_data_path, data_folder,img_name )) - self.mean_val
+		gaussian_map = get_gaussian_gt_label(gt_label, self.sigma, self.img_h, self.img_w)
+		return np.transpose(img,[2,0,1]),gaussian_map
+
+	def __len__(self):
+		return self.total_load_num* self.epoch
